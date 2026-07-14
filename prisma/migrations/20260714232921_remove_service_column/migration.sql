b611ada18376169d07cfcb87193e3c17a373ecab
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2026-07-15 02:38:57 +0300

    Remove service column migration

---
+++ prisma/migrations/20260714232921_remove_service_column/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `service` on the `providers` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "providers" DROP COLUMN "service";
