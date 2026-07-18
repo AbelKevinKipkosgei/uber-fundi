9d635a7131a6752d9daff638478bba8539e2d2a5
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2026-07-18 14:43:28 +0300

    Successful perform schema migration

---
+++ prisma/migrations/20260718114304_add_comment_likes/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "comment_likes" (
+    "id" TEXT NOT NULL,
+    "comment_id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "comment_likes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "comment_likes_comment_id_user_id_key" ON "comment_likes"("comment_id", "user_id");
+
+-- AddForeignKey
+ALTER TABLE "comment_likes" ADD CONSTRAINT "comment_likes_comment_id_fkey" FOREIGN KEY ("comment_id") REFERENCES "comments"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
